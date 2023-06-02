34df2e40617b10f163d7cd925b95ef663a03b29d
akash gothria <akash.gothria678@gmail.com>
2023-06-02 12:27:21 +0530

    Dependecies Injected for Project

---
+++ Data_DashBoard/src/test/java/com/app/dashboard/DataDashBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataDashBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
